14aceba2ec0629ce78e3d23ac96a4089c50a38cf
mrinmoydebnath <mrinmoydebn22@gmail.com>
2025-08-30 19:10:08 +0530

    Propeers find array intersection

--- propeers-problems/src/main/java/org/propeers/sorting/Sort.java
+++ propeers-problems/src/main/java/org/propeers/sorting/Sort.java
@@ -1,5 +1,7 @@
 package org.propeers.sorting;
 
+import java.util.ArrayList;
+
 public class Sort {
 
     public static void bubbleSort(int[] arr, int n) {
@@ -43,5 +45,21 @@ public class Sort {
             }
             arr[k] = val;
         }
+    }
+
+    public static ArrayList<Integer> findArrayIntersection(ArrayList<Integer> arr1, int n, ArrayList<Integer> arr2, int m) {
+        // Write Your Code Here.
+        ArrayList result = new ArrayList();
+        int i=0, j=0;
+        while(i<n && j<m) {
+            if (arr1.get(i) < arr2.get(j))  i++;
+            else if (arr1.get(i) > arr2.get(j)) j++;
+            else {
+                result.add(arr1.get(i));
+                i++;
+                j++;
+            }
+        }
+        return result;
     }
 }
