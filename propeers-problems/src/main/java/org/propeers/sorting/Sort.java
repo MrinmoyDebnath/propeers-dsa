422cbd2e64a7365a06d94f7df7b0214b07b04113
mrinmoydebnath <mrinmoydebn22@gmail.com>
2025-08-30 17:30:58 +0530

    Propeers selection sort

--- propeers-problems/src/main/java/org/propeers/sorting/Sort.java
+++ propeers-problems/src/main/java/org/propeers/sorting/Sort.java
@@ -14,4 +14,23 @@ public class Sort {
             }
         }
     }
+
+    public static void selectionSort(int arr[], int n) {
+        // Write your code here.
+        for(int i=0; i<n-1; i++) {
+            int minIndex = i;
+            for(int j = i+1; j<n; j++) {
+                if(arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        for (int i: arr) {
+            System.out.printf("%d ", i);
+        }
+    }
 }
